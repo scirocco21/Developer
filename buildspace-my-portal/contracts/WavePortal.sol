8715be810ac932249c8b8420612c877a0eee0353
scirocco21 <sgz40741@gmail.com>
2021-10-30 12:40:36 -0400

    Update WavePortal contract: users can send messages with wave

--- buildspace-my-portal/contracts/WavePortal.sol
+++ buildspace-my-portal/contracts/WavePortal.sol
@@ -5,22 +5,34 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-    address payable[] wavers;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+    
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+    
+    Wave[] waves;
 
     constructor() {
         console.log("Wassa Wassa Wassaaaap");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        wavers.push(payable(msg.sender));
-      
+        waves.push(Wave(msg.sender, _message, block.timestamp));      
         console.log("%s has waved at ya at block #%s", msg.sender, block.number);
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("Wohoo! We've collected %s waves from %s waver(s)!!", totalWaves, wavers.length);
+        console.log("Wohoo! We've collected %s waves!", totalWaves);
         return totalWaves;
     }
 
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
 }
